92bb466eeeb6b354e1e7825695bfba021bdb9207
Scott Lobdell <slobdell@google.com>
2021-01-25 13:39:18 -0800

    Revert "Add TCM (TCP Connection Management) support"
    This reverts commit 437a4680d0194a365ae556829801674af405c1a5.
    
    Bug: 178414480
    
    Change-Id: I82940a7443cc9946756ba346b28dea5cd9fbf2ca

--- okhttp/src/main/java/com/squareup/okhttp/ConnectionPool.java
+++ okhttp/src/main/java/com/squareup/okhttp/ConnectionPool.java
@@ -16,7 +16,6 @@
  */
 package com.squareup.okhttp;
 
-import com.squareup.okhttp.internal.Platform;
 import com.squareup.okhttp.internal.Internal;
 import com.squareup.okhttp.internal.RouteDatabase;
 import com.squareup.okhttp.internal.Util;
@@ -60,7 +59,6 @@ public final class ConnectionPool {
   private static final long DEFAULT_KEEP_ALIVE_DURATION_MS = 5 * 60 * 1000; // 5 min
 
   private static final ConnectionPool systemDefault;
-  private TcmIdleTimerMonitor mIdleMonitor;
 
   static {
     String keepAlive = System.getProperty("http.keepAlive");
@@ -88,7 +86,7 @@ public final class ConnectionPool {
       new LinkedBlockingQueue<Runnable>(), Util.threadFactory("OkHttp ConnectionPool", true));
 
   /** The maximum number of idle connections for each address. */
-  private int maxIdleConnections;
+  private final int maxIdleConnections;
   private final long keepAliveDurationNs;
   private Runnable cleanupRunnable = new Runnable() {
     @Override public void run() {
@@ -124,7 +122,6 @@ public final class ConnectionPool {
     if (keepAliveDuration <= 0) {
       throw new IllegalArgumentException("keepAliveDuration <= 0: " + keepAliveDuration);
     }
-    mIdleMonitor = new TcmIdleTimerMonitor(this);
   }
 
   public static ConnectionPool getDefault() {
@@ -326,18 +323,5 @@ public final class ConnectionPool {
 
   void setCleanupRunnableForTest(Runnable cleanupRunnable) {
     this.cleanupRunnable = cleanupRunnable;
-  }
-
-  /** Close all idle connections */
-  public synchronized void closeIdleConnections()
-  {
-    int oldMaxIdleConections = maxIdleConnections;
-    maxIdleConnections = 0;
-    try {
-      cleanupRunnable.run();
-    } catch (Exception err) {
-      Platform.get().logW("Unable to closeIdleConnections(): " + err);
-    }
-    maxIdleConnections = oldMaxIdleConections;
   }
 }
